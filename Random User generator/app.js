fd6713ca5b8ddc31728f82564b7b54f66031ac5a
Samuel Eiche <7samueleiche@gmail.com>
2022-01-29 15:39:39 +0200

    improve code

--- Random User generator/app.js
+++ Random User generator/app.js
@@ -1,47 +1,43 @@
-const allBtns = document.querySelectorAll('.info-btn');
-const btnUser = getElement('.btn');
-const display = getElement('.display');
+const infoButtons = document.querySelectorAll(".info-btn");
+const btnUser = getElement(".btn");
+const display = getElement(".display");
 const URL = "https://randomuser.me/api/";
 /* name and img */
-const displayUsername = getElement('.user-name');
-const displayImg = getElement('.user-img');
+const displayUsername = getElement(".user-name");
+const displayImg = getElement(".user-img");
 /* info btns */
-const emailBtn = getElement('.email-info')
-const phoneBtn = getElement('.phone-info');
-const ageBtn = getElement('.age-info');
-const countryBtn = getElement('.country-info');
-const usernameBtn = getElement('.username-info');
-const passwordBtn = getElement('.password-info');
+const emailBtn = getElement(".email-info");
+const phoneBtn = getElement(".phone-info");
+const ageBtn = getElement(".age-info");
+const countryBtn = getElement(".country-info");
+const usernameBtn = getElement(".username-info");
+const passwordBtn = getElement(".password-info");
+let lastActiveButtonType = "";
 
 /* getElement */
 function getElement(selection) {
-  const element = document.querySelector(selection)
-  if (element) return element
-  throw new Error(`Please check "${selection}", no such element exist`)
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(`Please check "${selection}", no such element exist`);
 }
 
 /* ainult üks highlightitud nupp - mis siin täpselt toimub??? */
-for (let i = 0; i < allBtns.length; i++) {
-  allBtns[i].addEventListener("click", function () {
-    document.querySelector('.active') ? document.querySelector('.active').classList.remove('active') : '';
-    this.classList.add('active');
+for (let i = 0; i < infoButtons.length; i++) {
+  infoButtons[i].addEventListener("click", function () {
+    document.querySelector(".active")
+      ? document.querySelector(".active").classList.remove("active")
+      : "";
+    this.classList.add("active");
+    lastActiveButtonType = this.dataset.type;
   });
 }
 
-/* On btn click it's more responsive */
-btnUser.addEventListener('click', () => {
-  btnUser.classList.add('btn-on-click')
-  setTimeout(() => {
-    btnUser.classList.remove('btn-on-click')
-  }, 100);
-});
-
 /* RANDOM USER GENERATOR API*/
-window.addEventListener('DOMContentLoaded', () => {
-  emailBtn.classList.add('active');
-  display.innerHTML = 'Click The Button'
+window.addEventListener("DOMContentLoaded", () => {
+  emailBtn.classList.add("active");
+  display.innerHTML = "Click The Button";
   /* fetch data  */
-  btnUser.addEventListener('click', async (e) => {
+  btnUser.addEventListener("click", async (e) => {
     try {
       const url = await fetch(URL);
       const data = await url.json();
@@ -51,44 +47,48 @@ window.addEventListener('DOMContentLoaded', () => {
       const country = response.location.country;
       const username = response.login.username;
       const password = response.login.password;
-      const firstName = response.name.first;
-      const lastName = response.name.last;
       const img = response.picture.large;
-      const { email, phone, name: { first, last } } = response;
-      const fullName = first + ' ' + last;
+      const {
+        email,
+        phone,
+        name: { first, last },
+      } = response;
+      const fullName = first + " " + last;
       console.log(response);
 
       /* display data */
-      emailBtn.addEventListener('click', () => {
+      emailBtn.addEventListener("click", () => {
         display.innerHTML = `Email: ${email}`;
-      })
-      phoneBtn.addEventListener('click', () => {
+      });
+      phoneBtn.addEventListener("click", () => {
         display.innerHTML = `Phone: ${phone}`;
-      })
-      ageBtn.addEventListener('click', () => {
+      });
+      ageBtn.addEventListener("click", () => {
         display.innerHTML = `Age: ${age}`;
-      })
-      countryBtn.addEventListener('click', () => {
-        display.innerHTML = `${fullName} lives in ${country}`;
-      })
-      usernameBtn.addEventListener('click', () => {
+      });
+      countryBtn.addEventListener("click", () => {
+        display.innerHTML = `Country: ${country}`;
+      });
+      usernameBtn.addEventListener("click", () => {
         display.innerHTML = `Username: ${username}`;
-      })
-      passwordBtn.addEventListener('click', () => {
+      });
+      passwordBtn.addEventListener("click", () => {
         display.innerHTML = `Password: ${password}`;
-      })
+      });
       /* display name, image and email on "random user" btn click */
       displayUsername.innerHTML = `${fullName}`;
       displayImg.src = img;
-      display.innerHTML = `Email: ${email}`;
+
+      display.innerHTML = `${lastActiveButtonType}: ${email}`;
 
       /* remove other active classes and add it to email class on "random user" btn click */
-      allBtns.forEach((btn) => {
-        if (btn.classList.contains('active')) {
-          btn.classList.remove('active')
-          emailBtn.classList.add('active')
-        }
-      })
+      infoButtons.forEach((btn) => {
+        btn.classList.remove("active");
+        const lastActiveButton = document.querySelector(
+          `[data-type="${lastActiveButtonType}"]`
+        );
+        lastActiveButton.classList.add("active");
+      });
       /* catch */
     } catch (error) {
       display.innerHTML = "There's an error";
